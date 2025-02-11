2a4f6ae3fbba58f917bdc761a8fdf01d21d01d35
knarimatsu <nariomasahisa@gmail.com>
2025-02-11 18:15:05 +0900

    PrismaService追加

---
+++ batch/src/services/prisma.service.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+export class PrismaService extends PrismaClient {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
