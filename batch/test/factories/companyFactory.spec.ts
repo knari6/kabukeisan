f52b2a61664f1c8f53e549357b4ef56c4b80fe90
knarimatsu <nariomasahisa@gmail.com>
2025-02-11 18:15:20 +0900

    CompanyテーブルのFactoryの追加

--- batch/test/factories/companyFactory.spec.ts
+++ batch/test/factories/companyFactory.spec.ts
@@ -1,12 +1,64 @@
-import { describe, it } from "vitest";
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { PrismaService } from "../../src/services/prisma.service";
+import { CompanyFactory } from "./companyFactory";
+import { Companies, Prisma } from "@prisma/client";
+import { DBHelper } from "../helper/db-helper";
 
 describe("CompanyFactory", () => {
+  const prismaService = new PrismaService();
+  const factory = new CompanyFactory(prismaService);
+
+  afterAll(async () => {
+    await prismaService.onModuleDestroy();
+  });
+
+  beforeEach(async () => {
+    await new DBHelper(prismaService).cleanUp();
+  });
+  afterAll(async () => {
+    await new DBHelper(prismaService).cleanUp();
+  });
   describe("create", () => {
+    let company: Companies | null;
+
     describe("パラメータがあるとき", () => {
-      it("指定した内容で登録されること", () => []);
+      let parameter: Prisma.CompaniesCreateInput;
+      beforeEach(async () => {
+        parameter = CompanyFactory.build();
+        await factory.create(parameter);
+        company = await prismaService.companies.findFirst({
+          where: { code: parameter.code, year: parameter.year },
+        });
+      });
+
+      it("指定した内容で登録されること", () => {
+        expect(company?.code.toString()).toBe(parameter.code.toString());
+        expect(company?.name).toBe(parameter.name);
+        expect(company?.year).toBe(parameter.year);
+        expect(company?.created_at.toString()).toBe(
+          parameter.created_at?.toString()
+        );
+        expect(company?.updated_at.toString()).toBe(
+          parameter.updated_at?.toString()
+        );
+      });
     });
     describe("パラメータがないとき", () => {
-      it("randomな値で登録されること", () => {});
+      beforeEach(async () => {
+        await factory.create({});
+        company = await prismaService.companies.findFirst({
+          orderBy: { id: Prisma.SortOrder.desc },
+        });
+      });
+
+      it("randomな値で登録されること", () => {
+        expect(company).not.toBeNull();
+        expect(company?.code.toString()).not.toBeNull();
+        expect(company?.name).not.toBeNull();
+        expect(company?.year).not.toBeNull();
+        expect(company?.created_at).not.toBeNull();
+        expect(company?.updated_at).not.toBeNull();
+      });
     });
   });
 });
--- batch/test/factories/companyFactory.ts
+++ batch/test/factories/companyFactory.ts
@@ -1,19 +1,31 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Companies, Prisma, PrismaClient } from "@prisma/client";
 import { Random } from "../../src/libs/random";
 import { randomUUID } from "crypto";
 
 export class CompanyFactory {
   public constructor(private readonly prismaClient: PrismaClient) {}
 
-  public build(
-    overrides: Partial<Prisma.CompaniesCreateInput>
+  public static build(
+    overrides: Partial<Prisma.CompaniesCreateInput> = {}
   ): Prisma.CompaniesCreateInput {
     const random = new Random();
     return {
       code: random.randomInt(1000, 9999).toString(),
       name: randomUUID(),
+      // 2999年超えることを想定していないため
       year: random.randomInt(1950, 2999).toString(),
-      account_datas: {},
+      created_at: random.randomDate(),
+      updated_at: random.randomDate(),
+      ...overrides,
     };
+  }
+
+  public async create(
+    overrides: Partial<Prisma.CompaniesCreateInput>
+  ): Promise<Companies> {
+    const input = CompanyFactory.build(overrides);
+    return this.prismaClient.companies.create({
+      data: input,
+    });
   }
 }
