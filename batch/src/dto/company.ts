f33a35cf23285df1987e0c35a666d1261ec61cce
knarimatsu <nariomasahisa@gmail.com>
2025-02-25 23:08:51 +0900

    Dto作成

--- batch/src/dto/company.ts
+++ batch/src/dto/company.ts
@@ -3,6 +3,7 @@ import { FinancialData } from "../libs/interfaces";
 type CompanyDtoType = {
   code: string;
   name: string;
+  year: string;
   createdAt: Date;
   updatedAt: Date;
 };
@@ -12,6 +13,7 @@ export class CompanyDto {
     return {
       code: data.information.code,
       name: data.information.companyName,
+      year: data.information.year,
       createdAt: new Date(),
       updatedAt: new Date(),
     };
