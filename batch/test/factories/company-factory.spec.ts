aa1fa49a01663a719b8581634710f1175f3f3aab
knarimatsu <nariomasahisa@gmail.com>
2025-03-02 15:08:58 +0900

    会社テーブルのファクトリー

--- batch/test/factories/company-factory.spec.ts
+++ batch/test/factories/company-factory.spec.ts
@@ -1,58 +1,58 @@
-// import { afterAll, beforeEach, describe, expect, it } from "vitest";
-// import { PrismaService } from "../../src/services/prisma.service";
-// import { CompanyFactory } from "./company-factory";
-// import { Companies, Prisma } from "@prisma/client";
-// import { DBHelper } from "../helper/db-helper";
+import { afterAll, beforeEach, describe, expect, it } from "vitest";
+import { PrismaService } from "../../src/services/prisma.service";
+import { CompanyFactory } from "./company-factory";
+import { Companies, Prisma } from "@prisma/client";
+import { DBHelper } from "../helper/db-helper";
 
-// describe("CompanyFactory", () => {
-//   const prismaService = new PrismaService();
-//   const factory = new CompanyFactory(prismaService);
+describe("CompanyFactory", () => {
+  const prismaService = new PrismaService();
+  const factory = new CompanyFactory(prismaService);
 
-//   afterAll(async () => {
-//     await prismaService.onModuleDestroy();
-//     await new DBHelper(prismaService).cleanUp();
-//   });
+  afterAll(async () => {
+    await prismaService.onModuleDestroy();
+    await new DBHelper(prismaService).cleanUp();
+  });
 
-//   describe("create", () => {
-//     let company: Companies | null;
+  describe("create", () => {
+    let company: Companies | null;
 
-//     describe("パラメータがあるとき", () => {
-//       let parameter: Prisma.CompaniesCreateInput;
-//       beforeEach(async () => {
-//         parameter = CompanyFactory.build();
-//         await factory.create(parameter);
-//         company = await prismaService.companies.findFirst({
-//           where: { code: parameter.code },
-//         });
-//       });
+    describe("パラメータがあるとき", () => {
+      let parameter: Prisma.CompaniesCreateInput;
+      beforeEach(async () => {
+        parameter = CompanyFactory.build();
+        await factory.create(parameter);
+        company = await prismaService.companies.findFirst({
+          where: { code: parameter.code },
+        });
+      });
 
-//       it("指定した内容で登録されること", () => {
-//         expect(company?.code.toString()).toBe(parameter.code.toString());
-//         expect(company?.name).toBe(parameter.name);
-//         expect(company?.createdAt.toString()).toBe(
-//           parameter.createdAt?.toString()
-//         );
-//         expect(company?.updatedAt.toString()).toBe(
-//           parameter.updatedAt?.toString()
-//         );
-//       });
-//     });
-//     describe("パラメータがないとき", () => {
-//       beforeEach(async () => {
-//         await factory.create({});
-//         company = await prismaService.companies.findFirst({
-//           orderBy: { id: Prisma.SortOrder.desc },
-//         });
-//       });
+      it("指定した内容で登録されること", () => {
+        expect(company?.code.toString()).toBe(parameter.code.toString());
+        expect(company?.name).toBe(parameter.name);
+        expect(company?.createdAt.toString()).toBe(
+          parameter.createdAt?.toString()
+        );
+        expect(company?.updatedAt.toString()).toBe(
+          parameter.updatedAt?.toString()
+        );
+      });
+    });
+    describe("パラメータがないとき", () => {
+      beforeEach(async () => {
+        await factory.create({});
+        company = await prismaService.companies.findFirst({
+          orderBy: { id: Prisma.SortOrder.desc },
+        });
+      });
 
-//       it("randomな値で登録されること", () => {
-//         expect(company).not.toBeNull();
-//         expect(company?.code.toString()).not.toBeNull();
-//         expect(company?.name).not.toBeNull();
+      it("randomな値で登録されること", () => {
+        expect(company).not.toBeNull();
+        expect(company?.code.toString()).not.toBeNull();
+        expect(company?.name).not.toBeNull();
 
-//         expect(company?.createdAt).not.toBeNull();
-//         expect(company?.updatedAt).not.toBeNull();
-//       });
-//     });
-//   });
-// });
+        expect(company?.createdAt).not.toBeNull();
+        expect(company?.updatedAt).not.toBeNull();
+      });
+    });
+  });
+});
