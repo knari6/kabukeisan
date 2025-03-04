5b229bbd40bf0397d0477e1b3bac440eda070d8a
knarimatsu <nariomasahisa@gmail.com>
2025-03-04 23:42:04 +0900

    financial-statement repository

--- batch/src/repository/financial-statement.ts
+++ batch/src/repository/financial-statement.ts
@@ -1,8 +1,45 @@
 import { PrismaClient } from "@prisma/client";
+import { FinancialData, QuarterType } from "../libs/interfaces";
+import { FinancialStatementDto } from "../dto/financial-statement";
 
 export class FinancialStatementRpository {
   private readonly prismaClient: PrismaClient;
   constructor(prismaClient: PrismaClient) {
     this.prismaClient = prismaClient;
+  }
+
+  public async write(
+    data: FinancialData,
+    year: string,
+    quarterType: QuarterType
+  ) {
+    const financialStatementDto = new FinancialStatementDto(
+      this.prismaClient,
+      data,
+      year,
+      quarterType
+    );
+    const company = await this.prismaClient.companies.findFirst({
+      where: {
+        code: data.information.code,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (!company) {
+      throw new Error("Company not found");
+    }
+    const financialStatement = financialStatementDto.dto();
+    await this.prismaClient.financialStatements.create({
+      data: {
+        ...financialStatement,
+        company: {
+          connect: {
+            id: company.id,
+          },
+        },
+      },
+    });
   }
 }
