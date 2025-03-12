eeaf3daacd8a6a6fbb61e5e18018d9e8ec09a0d8
knarimatsu <nariomasahisa@gmail.com>
2025-03-12 21:12:21 +0900

    資本支出のリポジトリの作成

--- batch/test/setup.ts
+++ batch/test/setup.ts
@@ -4,13 +4,18 @@ const prisma = new PrismaClient();
 
 process.on("beforeExit", async () => {
   try {
-    await prisma.$transaction([
-      prisma.cashFlowStatement.deleteMany(),
-      prisma.debtStatements.deleteMany(),
-      prisma.profitLossStatements.deleteMany(),
-      prisma.financialStatements.deleteMany(),
-      prisma.companies.deleteMany(),
-    ]);
+    await prisma
+      .$transaction([
+        prisma.cashFlowStatement.deleteMany(),
+        prisma.debtStatements.deleteMany(),
+        prisma.profitLossStatements.deleteMany(),
+      ])
+      .then(() => {
+        prisma.financialStatements.deleteMany();
+      })
+      .then(() => {
+        prisma.companies.deleteMany();
+      });
 
     // Prismaの接続を閉じる
     await prisma.$disconnect();
